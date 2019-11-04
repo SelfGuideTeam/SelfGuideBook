fcd7312ca5b790bd103846dac1e86d2ca2e1e301
rohyunge <rohyunge4@gmail.com>
2019-11-04 15:19:56 +0900

    vue

--- vue_Youngeui/FireStoreThree/app.js
+++ vue_Youngeui/FireStoreThree/app.js
@@ -17,9 +17,20 @@ function renderCafe(doc){
 }
 // getting data
 db.collection('cafes').get().then((snapshot) => {
-    snapshot.docs.forEach(element => {
-        renderCafe(element);
-    });
+    snapshot.docs.forEach(doc => {
+        // renderCafe(doc);
+        let li = document.createElement('li');
+        let name = document.createElement('span');
+        let city = document.createElement('span');
+
+        li.setAttribute('data-id', doc.id);
+        name.textContent = doc.id;
+        city.textContent = doc.data().city;
+
+        li.appendChild(name);
+        li.appendChild(city);
+        cafeList.appendChild(li);
+        }); 
     
 })
 // saving data
