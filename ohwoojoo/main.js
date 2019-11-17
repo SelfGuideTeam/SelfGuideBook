0e14a716aa7469022b9fe3213769da91fd2fe5ee
ohwoojoo <woojoo2121@gmail.com>
2019-11-17 16:25:08 +0900

    20191117

---
+++ ohwoojoo/main.js
@@ -0,0 +1,19 @@
+// 어느 페이지에서나 사용할 수 있도록
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import './plugins/firebase'
+import './plugins/axios'
+import './error'
+import './plugins/vue-toasted'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
